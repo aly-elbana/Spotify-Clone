67619c27f381b2f43ea3a8ce0d7862cb70bbfc83
Aly Elbana <alyelbana0@gmail.com>
2025-11-15 18:50:01 +0200

    finished frontend setupt (90%)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+import { Button } from "@/components/ui/button";
+
+export default function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <header className="flex justify-end p-4">
+      <SignedOut>
+        <SignInButton>
+          <Button variant="outline">Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton>
+          <Button variant="outline">Profile</Button>
+        </UserButton>
+      </SignedIn>
+    </header>
   );
 }
-
-export default App;
